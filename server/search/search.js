ca8bd93bd0e07ea8ab4c23b0cd59c67645a47f0f
aawantika <aawantika@gmail.com>
2015-03-07 20:47:03 -0500

    whoo search works

--- server/search/search.js
+++ server/search/search.js
@@ -79,7 +79,6 @@ search.prototype.searchProperty = function(req, res) {
     console.log(req.body.catsOk);
     console.log(req.body.dogsOk);
 
-
     propertyModel.aggregate([{
         $match: {
             // $or: [{
@@ -90,17 +89,99 @@ search.prototype.searchProperty = function(req, res) {
             //     // { distanceFromCampus: distanceFromCampus + 15 },
             //     // { distanceFromCampus: distanceFromCampus + 20 },
             // ],
-            // $or: [{
-            //     bedrooms: bedroomsInput
-            // }, {
-            //     bedrooms: bedroomsInput + 1
-            // }],
-            // bathrooms: bathrooms,
-            // housingType: housingType,
-            // price: price,
-            // length: length,
+            $or: [{
+                bedrooms: bedroomsInput
+            }, {
+                bedrooms: bedroomsInput + 1
+            }],
+            bathrooms: bathrooms,
+            housingType: housingType,
+            price: price,
+            length: length,
             catsOk: catsOk,
             dogsOk: dogsOk
+        }
+    }, {
+        $project: {
+            // distanceFromCampus: 1,
+            bedrooms: 1,
+            bathrooms: 1,
+            housingType: 1,
+            price: 1,
+            length: 1,
+            catsOk: 1,
+            dogsOk: 1,
+            score: {
+                $add: [
+                    // {
+                    //     $cond: [{
+                    //             $eq: ["$distanceFromCampus", distanceFromCampus]
+                    //         },
+                    //         20,
+                    //         3
+                    //     ]
+                    // }, 
+                    {
+                        "$cond": [{
+                                "$eq": ["$bedrooms", bedroomsInput]
+                            },
+                            10, {
+                                "$cond": [{
+                                        "$eq": ["$bedrooms", bedroomsInput + 1]
+                                    },
+                                    5,
+                                    0
+                                ]
+                            }
+
+                        ]
+                    }, {
+                        "$cond": [{
+                                "$eq": ["$bathrooms", bathrooms]
+                            },
+                            10,
+                            2
+                        ]
+                    }, {
+                        "$cond": [{
+                                "$eq": ["$housingType", housingType]
+                            },
+                            10,
+                            0
+                        ]
+                    }, {
+                        "$cond": [{
+                                "$eq": ["$price", price]
+                            },
+                            10,
+                            1
+                        ]
+                    }, {
+                        "$cond": [{
+                                "$eq": ["$length", length]
+                            },
+                            10,
+                            5
+                        ]
+                    }, {
+                        "$cond": [
+                            "$catsOk",
+                            10,
+                            0
+                        ]
+                    }, {
+                        "$cond": [
+                            "$dogsOk",
+                            10,
+                            0
+                        ]
+                    },
+                ]
+            }
+        }
+    }, {
+        $sort: {
+            score: -1
         }
     }], function(err, result) {
         if (err) {
@@ -111,120 +192,6 @@ search.prototype.searchProperty = function(req, res) {
             res.status(200).send(result);
         }
     });
-
-    // propertyModel.aggregate([{
-    //     $match: {
-    //         // $or: [{
-    //         //         distanceFromCampus: distanceFromCampus
-    //         //     }
-    //         //     // { distanceFromCampus: distanceFromCampus + 5 },
-    //         //     // { distanceFromCampus: distanceFromCampus + 10 },
-    //         //     // { distanceFromCampus: distanceFromCampus + 15 },
-    //         //     // { distanceFromCampus: distanceFromCampus + 20 },
-    //         // ],
-    //         $or: [{
-    //             bedrooms: bedroomsInput
-    //         }, {
-    //             bedrooms: bedroomsInput + 1
-    //         }],
-    //         bathrooms: bathrooms,
-    //         housingType: housingType,
-    //         price: price,
-    //         length: length,
-    //         catsOk: catsOk,
-    //         dogsOk: dogsOk
-    //     }
-    // }, {
-    //     $project: {
-    //         // distanceFromCampus: 1,
-    //         bedrooms: 1,
-    //         bathrooms: 1,
-    //         housingType: 1,
-    //         price: 1,
-    //         length: 1,
-    //         catsOk: 1,
-    //         dogsOk: 1,
-    //         score: {
-    //             $add: [
-    //                 // {
-    //                 //     $cond: [{
-    //                 //             $eq: ["$distanceFromCampus", distanceFromCampus]
-    //                 //         },
-    //                 //         20,
-    //                 //         3
-    //                 //     ]
-    //                 // }, 
-    //                 {
-    //                     "$cond": [{
-    //                             "$eq": ["$bedrooms", bedroomsInput]
-    //                         },
-    //                         10, {
-    //                             "$cond": [{
-    //                                     "$eq": ["$bedrooms", bedroomsInput + 1]
-    //                                 },
-    //                                 5,
-    //                                 0
-    //                             ]
-    //                         }
-
-    //                     ]
-    //                 }, {
-    //                     "$cond": [{
-    //                             "$eq": ["$bathrooms", bathrooms]
-    //                         },
-    //                         10,
-    //                         2
-    //                     ]
-    //                 }, {
-    //                     "$cond": [{
-    //                             "$eq": ["$housingType", housingType]
-    //                         },
-    //                         10,
-    //                         0
-    //                     ]
-    //                 }, {
-    //                     "$cond": [{
-    //                             "$eq": ["$price", price]
-    //                         },
-    //                         10,
-    //                         1
-    //                     ]
-    //                 }, {
-    //                     "$cond": [{
-    //                             "$eq": ["$length", length]
-    //                         },
-    //                         10,
-    //                         5
-    //                     ]
-    //                 }, {
-    //                     "$cond": [
-    //                         "$catsOk",
-    //                         10,
-    //                         0
-    //                     ]
-    //                 }, {
-    //                     "$cond": [
-    //                         "$dogsOk",
-    //                         10,
-    //                         0
-    //                     ]
-    //                 },
-    //             ]
-    //         }
-    //     }
-    // }, {
-    //     $sort: {
-    //         score: -1
-    //     }
-    // }], function(err, result) {
-    //     if (err) {
-    //         console.log(err);
-    //         res.status(400).send(err);
-    //     } else {
-    //         console.log(result);
-    //         res.status(200).send(result);
-    //     }
-    // });
 
 
 
